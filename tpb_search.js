cf09de6e42dbf6f073c997d6ddca84f0e3b3e05a
Progamersang <156645843+Progamersang@users.noreply.github.com>
2025-08-15 18:06:22 +0700

    Create tpb_search.js

---
+++ tpb_search.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+const { getMovieMetadata, getSeriesMetadata } = require('./test1');
+
+const TPB_URL = process.env.TPB_URL || 'https://apibay.org';
+
+function formatSize(bytes) {
+    if (bytes === 0) return "0 Bytes";
+    const k = 1024;
+    const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + " " + sizes[i];
+}
+
+// Tìm torrent theo IMDb ID từ TPB
+async function searchTPBByImdb(imdbId) {
+    const response = await axios.get(`${TPB_URL}/q.php?q=${imdbId}`);
+    return response.data.map(t => ({
+        title: t.name,
+        info_hash: t.info_hash,
+        size: formatSize(parseInt(t.size)),
+        id_imdb: t.imdb,
+        seeders: t.seeders,
+        leechers: t.leechers
+    }));
+}
+
+module.exports = { searchTPBByImdb }
